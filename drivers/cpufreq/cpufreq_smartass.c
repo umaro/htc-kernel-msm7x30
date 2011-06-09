fb24d7dc21fa8ba1629b1e5d7490247d09066027
Umaro <Umaro@narshe.net>
2011-06-09 09:09:03 -0700

    Fix 'smartass' min awake freq

--- drivers/cpufreq/cpufreq_smartass.c
+++ drivers/cpufreq/cpufreq_smartass.c
@@ -110,7 +110,7 @@ static unsigned int sleep_wakeup_freq;
  * go below this frequency.
  * Set awake_min_freq=0 to disable this behavior.
  */
-#define DEFAULT_AWAKE_MIN_FREQ 979200
+#define DEFAULT_AWAKE_MIN_FREQ 368640
 static unsigned int awake_min_freq;
 
 /*
